7bca2081d862a55d9e7a69b1c9fe8c8114ed39b8
Jasmine Mills <jazzrmills@gmail.com>
2022-08-23 10:42:06 +0100

    add titles

--- src/containers/releases/ReleasesContainer.js
+++ src/containers/releases/ReleasesContainer.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { Helmet } from 'react-helmet'
 import hello_darkness from '../../assets/releases/hello_darkness.jpeg'
 import silence from '../../assets/releases/silence.jpeg'
+import splintered from '../../assets/releases/splintered.jpeg'
 import Accordion from 'react-bootstrap/Accordion'
 import ReactPlayer from 'react-player'
 import '../releases/Releases.css'
@@ -14,10 +15,21 @@ const ReleasesContainer = () => {
     <Helmet>
       <title>Matthew Collings</title>
     </Helmet>
-
+    <div className='releases-full'>
     <div className="section-1">
       <div className="img-info-flex">
+      <h1 className='section-1-header'>Hello Darkness</h1>
       <img width = '400px' src = {hello_darkness} alt = "Hello Darkness Cover" />
+      <ReactPlayer className = "react-player"
+      url = 'https://soundcloud.com/denovali/dag-rosenqvist-matthew-collings-streets?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
+      width = '100px'
+      height = '100px'
+      config={{
+     soundcloud: {
+      options: { 
+        sharing: false }
+          }}}
+      />
       <Accordion defaultActiveKey="1">
         <Accordion.Item eventKey="0">
           <Accordion.Header className='accordian-header-1'>Info</Accordion.Header>
@@ -34,21 +46,24 @@ const ReleasesContainer = () => {
           </Accordion.Body>
         </Accordion.Item>
       </Accordion> 
-      <ReactPlayer 
-      url = 'https://soundcloud.com/denovali/dag-rosenqvist-matthew-collings-streets?utm_source=mcollingsmusic.com&utm_campaign=wtshare&utm_medium=widget&utm_content=https%253A%252F%252Fsoundcloud.com%252Fdenovali%252Fdag-rosenqvist-matthew-collings-streets'
-      width = '30%'
-      height = '50px'
-      config={{
-     soundcloud: {
-      playerVars: { showinfo: 1 }
-    }
-      />
       </div>
     </div>
+
     <div className="section-2">
       <div className="img-info-flex">
-      <img width = '400px' src = {silence} alt = "Silence Is A Rhythm Too Cover" />
-      <Accordion defaultActiveKey="1">
+      <h1 className='section-2-header'>Silence Is A Rhythm Too</h1>
+        <img width = '400px' src = {silence} alt = "Silence Is A Rhythm Too Cover" />
+        <ReactPlayer className = "react-player"
+          url = 'https://soundcloud.com/denovali/matthew-collings-toms?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
+          width = '100px'
+          height = '100px'
+          config={{
+             soundcloud: {
+                options: { 
+                sharing: false }
+              }}}
+          />
+        <Accordion defaultActiveKey="1">
         <Accordion.Item eventKey="0">
           <Accordion.Header className='accordian-header-2'>Info</Accordion.Header>
           <Accordion.Body className = 'accordian-body-2'>
@@ -60,6 +75,35 @@ const ReleasesContainer = () => {
         </Accordion.Item>
       </Accordion> 
       </div>
+    </div>
+    <div className="section-1">
+      <div className="img-info-flex">
+      <h1 className='section-1-header'>Splintered Instruments</h1>
+        <img width = '400px' src = {splintered} alt = "Splintered Instruments Cover" />
+        <ReactPlayer  className = 'react-player'
+          url = 'https://soundcloud.com/denovali/matthew-collings-vasilia?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
+          width = '100px'
+          height = '100px'
+          config={{
+             soundcloud: {
+                options: { 
+                sharing: false,
+                controls: false }
+              }}}
+          />
+        <Accordion defaultActiveKey="1">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header className='accordian-header-1'>Info</Accordion.Header>
+          <Accordion.Body className = 'accordian-body-1'>
+          I wanted to create a more direct sound to involve a much grander and more dangerous range of emotions. The composition process of “Splintered Instruments” came out of personally reckoning with two contrary forces, something vicious and unrelenting challenging with a soft and kind energy – getting to their core and the dynamic interrelation to one another. Filtered through melody and vaguely resembling songs, driven by nested rhythm patterns, one can imagine that the first sound Collings picked up on as a leading impulse “was like Sonic Youth’s ‘The Diamond Sea’ meeting ‘Music for 18 Musicians’. I wanted to take songs and kind of dissolve them and then bring them back again, with everything just being on the edge of these various types of structures.”
+          <br/>
+          He worked closely with Ben Frost on the creation of “Splintered Instruments”, who took influence in the physicality and size of sound and helped to achieve him to reach into the deepest parts of human nature, pulling it out in a hybrid-pop-experimental-noise form. According to Collings, the record is “a revolt against machines” understanding the sense of precision as something pretty unnatural. He avoids working with computers only using them for convenience, not as a principle. 90% of the record is organic sourced from or played on acoustic instruments or objects, being drawn to sound of every kind which is more human, more imperfect in its subtle variations. Splintered Instruments '…is not about noise or the decibel levels – it’s about the sheer uncompromised approach to raw sound design' (Headphone Commute).
+
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion> 
+      </div>
+    </div>
     </div>
     </>
   )
