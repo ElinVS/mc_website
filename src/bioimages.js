b16a8999da676004a9739f55c5249112dc762686
matt collings <mc@krotosltd.com>
2022-08-30 22:46:54 +0100

    quotes animation

--- src/bioimages.js
+++ src/bioimages.js
@@ -1,5 +1,28 @@
-import image1 from './assets/thirdmind/matt_laptop.jpg'
-import image2 from  './assets/thirdmind/matt_laptop.jpg'
+const quoteArray = [
+    'deeply unsettling THE WIRE',
+
+    'especially inspiring and stunningly crafted EDINBURGH GUIDE',
+
+    'this is art-house ambience aiming to be something a lot different to the norm, and succeeding...in an era where everything seems a copy of a copy, his work has a freshness to it METRO',
+
+'by now there’s no doubt that Matthew Collings is an innovative composer on the scale of Ben Frost, Greg Haines, Tim Hecker, Nico Muhly, and so on. His work has become gradually outstanding, and with Silence is a Rhythm Too he’s blazed his own path through contemporary music, recording an album that is as emotionally intense as it is complex. (David Murrieta - A CLOSER LISTEN)',
+
+'by turns politically muscular and poignantly nuanced, Collings excellent piece is a heartening reminder of the quality of new music being created and performed in Scotland today HERALD SCOTLAND',
+
+'especially inspiring and stunningly crafted EDINBURGH GUIDE',
+
+'an elemental whorl of electro-acoustic (de)composition BOOMKAT.COM',
+
+'creates a world of its own that envelopes the listener and provides a playground for the imagination, a theatre stage in surround sound FLUID RADIO',
+
+'the music on Splintered Instruments is both rough and beautiful.This is not about noise or the decibel levels (although I do recommend you listen to this album loud) – it’s about the sheer uncompromised approach to raw sound design...The final result is somewhat unclassifiable. - HEADPHONE COMMUTE',
+
+'we can only raise a toast in gratitude that such music is being made despite its unfortunately underground status A CLOSER LISTEN',
+
+'an album that is intensely emotional and sonically disturbing, but also creates a vein of melancholy and accessibility that ambient music rarely sees THE DEATH OF CDS'
+
+]
 
 
-export default [image1, image2]
+export default quoteArray
+
--- src/containers/bio/BioContainer.js
+++ src/containers/bio/BioContainer.js
@@ -4,11 +4,23 @@ import { Helmet } from 'react-helmet'
 import '../bio/Bio.css'
 
 import bioimages from '../../bioimages'
+import quoteArray from '../../bioimages'
 
 
 const BioContainer = ({setCursorVariant}) => {
 
-  // 
+
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect (() => {
+    const imageId = setInterval (() => {
+      setCurrentImage((i) => {
+        return i === 10 ? 0 : i +1;
+      })
+    }, 8000)
+    return () => clearInterval(imageId)
+  }, []);
 
 
   return (
@@ -28,6 +40,10 @@ const BioContainer = ({setCursorVariant}) => {
         <h1 id='h1-title-bio'>MATTHEW </h1>
       </header>
 
+      <div className='press-text-container'>
+        {quoteArray[currentImage]}
+      </div>
+
       <div className='bio-container'>
         <div className='portrait-img-container'>
         </div>
@@ -38,12 +54,17 @@ const BioContainer = ({setCursorVariant}) => {
         </div>
       </div>
 
-      <div className='press-text-container'>
-        <p>'deeply unsettling' THE WIRE</p>
+     
+
+      <div className='bio-container'>
+        <div className='portrait-img-container'>
+        </div>
+        <div className='portrait-text-container'>
+          <p>Collings's work contrasts the crushed guitars and textures of My Bloody Valentine or Sonic Youth with structures more akin to contemporary classical composers like Steve Reich or David Lang. In his powerful live performances, he uses the amplifier as an instrument and the guitar as a control device for innovative digital processes. Using numbers of 'prepared amplifiers', he coaxes unique sounds by placing bells, rice and wood directly onto the speaker cone and manipulating the physical vibrations with his fingers, producing a highly visual and physical performance. This music is textural, melodic and often sonically overwhelming.
+          </p>
+          <p>His 'audio-visual opera' with Jules Rawlinson, for strings, clarinet, electronics and real-time visuals, 'A Requiem for Edward Snowden' was premiered to a sell-out crowd in October 2014, and has been performed at Glasgow CCA, Edinburgh Fringe Festival (as part of Made in Scotland Showcase 2015), Aberdeen Sound Festival and Utrecht's Gaudeamus Muziekweek (NL).</p>
+        </div>
       </div>
-
-
-
     
     </section>
     
@@ -56,19 +77,12 @@ export default BioContainer
 
 
 
-// const [currentImage, setCurrentImage] = useState(0);
+// 
 
 
 
 
-  // useEffect (() => {
-  //   const imageId = setInterval (() => {
-  //     setCurrentImage((i) => {
-  //       return i === 1 ? 0 : i +1;
-  //     })
-  //   }, 5000)
-  //   return () => clearInterval(imageId)
-  // }, []);
+ 
 
     {/* <div className='bio-image-container'>
         <img src={bioimages[currentImage]} className='bio-image'/>
